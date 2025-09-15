3857aff5f0a8a3f5fc1e8fe0be637ee63c8d2db5
FrancoRosa <km115.franco@gmail.com>
2025-09-15 11:19:51 -0500

    Add dependencies to installer

--- setup.sh
+++ setup.sh
@@ -42,6 +42,20 @@ install_dependency "curl" "sudo apt-get install -y curl "
 install_dependency "git" "sudo apt-get install  -y git"
 install_dependency "node" "sudo apt install nodejs -y" "curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash - "
 install_dependency "pm2" "sudo npm i -g pm2"
+install_dependency "tailscale" "curl -fsSL https://tailscale.com/install.sh | sh"
+install_dependency "brave-browser" "curl -fsS https://dl.brave.com/install.sh | sh"
+install_dependency "anydesk" "sudo apt install ca-certificates curl apt-transport-https \
+sudo install -m 0755 -d /etc/apt/keyrings \
+sudo curl -fsSL https://keys.anydesk.com/repos/DEB-GPG-KEY -o /etc/apt/keyrings/keys.anydesk.com.asc \
+sudo chmod a+r /etc/apt/keyrings/keys.anydesk.com.asc"
+
+# Add the AnyDesk apt repository
+echo "deb [signed-by=/etc/apt/keyrings/keys.anydesk.com.asc] https://deb.anydesk.com all main" | sudo tee /etc/apt/sources.list.d/anydesk-stable.list > /dev/null
+
+# Update apt caches and install the AnyDesk client
+sudo apt update
+sudo apt install anydesk"
+
 npm i serialport
 sudo apt-get install -y openssh-server
 sudo systemctl enable ssh --now
@@ -55,10 +69,27 @@ sudo pm2 start ~/api.js --restart-delay 5000 --max-memory-restart 300M --name "r
 sudo pm2 start ~/mosaic.js --restart-delay 5000 --max-memory-restart 300M --name "mosaic"
 sudo pm2 stop rtk
 sudo pm2 save
-curl -fsSL https://tailscale.com/install.sh | sh
 
-# sudo sed -i '/.*WaylandEnable=.*/ s/.*/WaylandEnable=false/' "/etc/gdm3/custom.conf"
-# sudo systemctl restart gdm3
-# echo "Mortenson123" | sudo anydesk --set-password
+# set unattended password
+echo -n "Mortenson123" | sudo anydesk --set-password
+
+# disable wayland on ubuntu for anydesk
+sudo sed -i '/.*WaylandEnable=.*/ s/.*/WaylandEnable=false/' "/etc/gdm3/custom.conf"
+sudo systemctl restart gdm3
+
+# gnome settings
+gsettings set org.gnome.shell favorite-apps "['org.gnome.Terminal.desktop', 'anydesk.desktop', 'brave-browser.desktop']"
+gsettings set org.gnome.desktop.interface enable-animations false
+
+
+# brave settings
+jq '.homepage = "https://tt--pile-placer.netlify.app" 
+    | .homepage_is_newtabpage = false
+    | .session.startup_urls = ["https://tt--pile-placer.netlify.app"]
+    | .session.restore_on_startup = 4' \
+~/.config/BraveSoftware/Brave-Browser/Default/Preferences \
+> /tmp/prefs && mv /tmp/prefs ~/.config/BraveSoftware/Brave-Browser/Default/Preferences
+
+
 rm z
 echo "${grn}... done${rst}"
